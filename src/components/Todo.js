9b46b713852bef1c35bf867c92b5c0a63f07377f
zhuyan08271521 <15864681521@163.com>
2019-06-12 00:43:07 +0800

    完成loading效果和state控制样式

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -61,10 +61,11 @@ class Todo extends Component {
 
   openUpdate = (index) => {
       this.setState({
-          index: index
+          index: index,
+          showEdit: true
       });
       this.input.value = this.state.items[index].text;
-      document.getElementById("edit").style.display = "";
+
   }
 
   updateDate(flag) {
@@ -74,38 +75,56 @@ class Todo extends Component {
               let templist = this.state.items;
               let index = this.state.index;
               templist[index].text = inpVal;
-              this.setState({
+              this.setState ({
                   items: templist
-            });
+              });
           }
       }
-      document.getElementById("edit").style.display = "none";
-  }
-
-  render() {
-      const { items, show, text } = this.state
-      return (
-          <div>
-              <h3>TODO</h3>
-              <TodoList items = { items }
-                  show = { show }
-                  deleteDate = { this.deleteDate }
-                  selectDate = { this.selectDate }
-                  changeFlag = { this.changeFlag }
-                  openUpdate = { this.openUpdate }/>
-              <button onClick = {this.selectDate.bind(this,0)}>全部</button>
-              <button onClick = {this.selectDate.bind(this,1)}>已完成</button>
-              <button onClick = {this.selectDate.bind(this,2)}>未完成</button>
-              <Forms handleSubmit = {this.handleSubmit} text={text} handleChange={this.handleChange}/>
-              <div id='edit' style={{display: 'none'}}>
-                  <input type='text' ref={input => this.input = input}/><br/>
-                  <button onClick={this.updateDate.bind(this,true)}>确定</button>
-                  <button onClick={this.updateDate.bind(this,false)}>取消</button>
-              </div>
-          </div>
-        );
+            this.setState({
+                showEdit: false
+            })
     }
-}
+    componentDidMount(){
+        let data = [{
+            text: 'abc',
+            flag: true,
+            id: 1
+        },{
+            text: 'def',
+            flag: false,
+            id: 2
+        }]
+        setTimeout(() => {
+            this.setState({
+                items: data
+            });
+        },3000)
+    }
+    render() {
+        const { items, show, text, showEdit } = this.state
+        return (
+            <div>
+                <h3>TODO</h3>
+                <div className = {items.length!==0 ? 'closeLoading' : ''}>loading......</div>
+                <TodoList items = { items }
+                    show = { show }
+                    deleteDate = { this.deleteDate }
+                    selectDate = { this.selectDate }
+                    changeFlag = { this.changeFlag }
+                    openUpdate = { this.openUpdate }/>
+                <button onClick = {this.selectDate.bind(this,0)}>全部</button>
+                <button onClick = {this.selectDate.bind(this,1)}>已完成</button>
+                <button onClick = {this.selectDate.bind(this,2)}>未完成</button>
+                <Forms handleSubmit = {this.handleSubmit} text = {text} handleChange = {this.handleChange}/>
+                <div className={showEdit ? 'showEdit' : 'closeEdit'}>
+                    <input type='text' ref={input => this.input = input}/><br/>
+                    <button onClick={this.updateDate.bind(this,true)}>确定</button>
+                    <button onClick={this.updateDate.bind(this,false)}>取消</button>
+                </div>
+            </div>
+            );
+        }
+    }
 
 
 export default Todo
